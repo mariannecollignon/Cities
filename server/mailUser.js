5135423c8d3d7172d447e5c6b275cc33407069e4
Marianne <marianne.c62@gmail.com>
2017-03-01 11:31:16 +0100

    Test

--- server/mailUser.js
+++ server/mailUser.js
@@ -2,6 +2,9 @@ Meteor.startup(function () {
     process.env.MAIL_URL = 'smtp://meteorlensdundalk@gmail.com:meteorEire@smtp.gmail.com:587';
 });
 
+
+
+
 Accounts.config({
     sendVerificationEmail: true,
     forbidClientAccountCreation: false
